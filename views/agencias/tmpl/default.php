9df13f5fc7cb86bf52e63c1de6954ae61fee3c43
alexzer0 <alexanderc4st@gmail.com>
2016-04-15 10:16:45 -0500

    cambios

--- views/agencias/tmpl/default.php
+++ views/agencias/tmpl/default.php
@@ -1,10 +1,4 @@
-<div class="gantry-width-10 gantry-width-block"> </div>
-<div class="gantry-width-90 gantry-width-block"><a href="#"><em class="bold">+1 (555) 555-555-5555</em></a><br /> <span><em class="bold nobold">+1 (555) 555-555-5556</em></span></div>
-<div class="clear largemarginbottom"> </div>
-<div class="gantry-width-10 gantry-width-block"> </div>
-<div class="gantry-width-90 gantry-width-block"><a href="#"><em class="bold"><strong>Graffito Template LLC</strong></em></a><br /> <em class="bold nobold"> 123 Joomla! Boulevard<br /> Seattle, WA 00000, USA </em></div>
-<div class="clear largemarginbottom"> </div>
-<div class="gantry-width-10 gantry-width-block hidden-tablet"> </div>
-<div class="gantry-width-90 gantry-width-block hidden-tablet">
-<p><em class="bold">noreply@domain.com</em></p>
+<div class="row">
+<div class="span4">...column A......</div>
+<div class="span8">...column B.... </div>
 </div>
