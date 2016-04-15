824a62c46a3742c216dbe462f0aa30527633f4c9
alexzer0 <alexanderc4st@gmail.com>
2016-04-15 10:14:31 -0500

    probando columnas

--- views/agencias/tmpl/default.php
+++ views/agencias/tmpl/default.php
@@ -1,27 +1,10 @@
-<div class="row">
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-</div>
-<div class="row">
-  <div class="col-md-8">.col-md-8</div>
-  <div class="col-md-4">.col-md-4</div>
-</div>
-<div class="row">
-  <div class="col-md-4">.col-md-4</div>
-  <div class="col-md-4">.col-md-4</div>
-  <div class="col-md-4">.col-md-4</div>
-</div>
-<div class="row">
-  <div class="col-md-6">.col-md-6</div>
-  <div class="col-md-6">.col-md-6</div>
+<div class="gantry-width-10 gantry-width-block"> </div>
+<div class="gantry-width-90 gantry-width-block"><a href="#"><em class="bold">+1 (555) 555-555-5555</em></a><br /> <span><em class="bold nobold">+1 (555) 555-555-5556</em></span></div>
+<div class="clear largemarginbottom"> </div>
+<div class="gantry-width-10 gantry-width-block"> </div>
+<div class="gantry-width-90 gantry-width-block"><a href="#"><em class="bold"><strong>Graffito Template LLC</strong></em></a><br /> <em class="bold nobold"> 123 Joomla! Boulevard<br /> Seattle, WA 00000, USA </em></div>
+<div class="clear largemarginbottom"> </div>
+<div class="gantry-width-10 gantry-width-block hidden-tablet"> </div>
+<div class="gantry-width-90 gantry-width-block hidden-tablet">
+<p><em class="bold">noreply@domain.com</em></p>
 </div>
