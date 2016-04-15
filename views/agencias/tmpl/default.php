5677c4ae6dbb9e380effc97c2e6253172b43114f
alexzer0 <alexanderc4st@gmail.com>
2016-04-15 10:06:57 -0500

    probando bootstrap

--- views/agencias/tmpl/default.php
+++ views/agencias/tmpl/default.php
@@ -1,54 +1,27 @@
-<form name="frmLogin" id="frmLogin" method="post" action="vLogin.php">
-
-<table align="center" cellpadding="5" cellspacing="0" width="40%">
-
-<caption>
-
-Entrar a Página de Agencia Mayoristas
-
-</caption>
-
-	<tr class="odd">
-
-		<td colspan="2">
-</td>
-
-	</tr>
-
-	<tr>
-
-		<td>Usuario:</td>
-
-	  <td><input type="text" name="user" id="inputtext" />&nbsp;</td>		
-
-	</tr>
-
-	<tr>
-
-		<td>Contraseña:</td>
-
-		<td><input type="password" name="pass" id="inputtext" />&nbsp;</td>
-
-	</tr>
-
-	<tr>
-
-		<td colspan="2" align="right"><input type="submit" value="Entrar " id="inputbuton" /></td>
-
-	</tr>
-
-	<tr class="odd">
-
-		<td colspan="2" align="center" bgcolor="#D3E7CF">
-
-		<strong>Si su agencia no esta registrada Solicite su Acceso </strong><br /><br />
-
-		<a href="newpass.php"></a>
-
-		</td>
-
-	</tr>
-
-</table>
-
-</form>
+<div class="row">
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+</div>
+<div class="row">
+  <div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+</div>
+<div class="row">
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4">.col-md-4</div>
+</div>
+<div class="row">
+  <div class="col-md-6">.col-md-6</div>
+  <div class="col-md-6">.col-md-6</div>
+</div>
